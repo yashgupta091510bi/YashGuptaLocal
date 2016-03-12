fa38d90fd0939d9e57a3a2b124775c3f93dc6e89
Yash Gupta <yash.gupta091510bi@gmail.com>
2016-03-13 05:13:36 +0530

    index,show,edit,new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
+ def index
+@article=Article.all
+ end
+
   def new
     @article = Article.new
   end
 
+  def edit
+
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice]= "Aricle was successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
   def create
     
 @article =Article.new(article_params)
